1b61892833c1a4f9c23e2a6646b2990d296abcd1
djmets0308 <jeong.danny0308@gmail.com>
2020-06-09 22:54:14 -0400

    add Startool

---
+++ Startool/src/me/Danny/Startool/MainStartool.java
@@ -0,0 +1,128 @@
+package me.Danny.Startool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class MainStartool extends JavaPlugin implements Listener {
+
+	public List<String> list = new ArrayList<String>(); 
+	
+	@Override
+	public void onEnable() {
+
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(label.equalsIgnoreCase("startool")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("sorry wrong sender");
+				return true;
+			}
+			Player player = (Player) sender;
+			if (player.getInventory().firstEmpty() == -1) {
+				//inventory is full
+				Location loc = player.getLocation();
+				World world = player.getWorld();
+				
+				world.dropItemNaturally(loc, getItem());
+				player.sendMessage(ChatColor.RED + "u dropped the gift bruh");
+				return true;
+			}
+			player.getInventory().addItem(getItem());
+			player.sendMessage(ChatColor.GOLD+ "poseidon summons u! poggers");
+			return true;
+			
+		}
+		return false;
+	}
+	
+	public ItemStack getItem() {
+		ItemStack trident = new ItemStack(Material.TRIDENT);
+		ItemMeta meta = trident.getItemMeta();
+		
+		meta.setDisplayName(ChatColor.GREEN+ "poseidons dong");
+		List <String> lore = new ArrayList<String>();
+		lore.add("");
+		lore.add(ChatColor.GOLD + "&7(Right Click)  Spawn minions");
+		lore.add(ChatColor.GOLD+ "(Left Click)  Shoot fireball");
+
+		meta.setLore(lore);
+		
+		meta.addEnchant(Enchantment.LOYALTY, 10, true);
+		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		meta.setUnbreakable(true);
+		
+		trident.setItemMeta(meta);
+		
+		return trident;
+	}
+	
+	@EventHandler()
+	public void onClick(PlayerInteractEvent event) {
+		if(event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.TRIDENT))
+			if(event.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasLore()) {
+				Player player = (Player) event.getPlayer();
+				//right click
+				if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+					if (!list.contains(player.getName()))
+						list.add(player.getName());
+						return;
+				}
+				//left click
+				if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+					player.launchProjectile(Fireball.class);
+				}
+			}
+	if (list.contains(event.getPlayer().getName())) {
+		list.remove(event.getPlayer().getName());
+		}
+	}
+
+	@EventHandler()
+	public void onLand(ProjectileHitEvent event) {
+		if (event.getEntityType() == EntityType.TRIDENT) {
+			if (event.getEntity().getShooter() instanceof Player) {
+				Player player = (Player) event.getEntity().getShooter();
+				if (list.contains(player.getName())) {
+					//spawn zombies
+					Location loc = event.getEntity().getLocation();
+					loc.setY(loc.getY()+1);
+					
+					for(int i =1; i<4 ; i++) {
+					loc.getWorld().spawnEntity(loc, EntityType.LIGHTNING);
+					loc.setX(loc.getX()+i);
+					}
+				}
+			}
+		}
+	}
+  
+}
